3b5c41205d74fcf1a96c12e3c2ddbcbacaa899f1
Maxim Vainikka <vainikka@users.noreply.github.com>
2020-09-26 07:49:29 +1000

    Initial Commit

---
+++ example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  example
+//
+//  Created by Максим on 26.09.2020.
+//  Copyright © 2020 SolisOrtum. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
